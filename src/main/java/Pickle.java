feec2e114df9d137343f424d1b210fa752477f40
tevyntan <tevyntanyb@gmail.com>
2025-08-21 17:06:27 +0800

    Add the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Pickle.java
+++ src/main/java/Pickle.java
@@ -3,8 +3,9 @@ import java.util.*;
 public class Pickle {
     private static String line = "________________________________________________";
     public static void main(String[] args) {
-
+        String[] list = new String[100];
         Scanner sc = new Scanner(System.in);
+        int counter = 0;
 
         System.out.println(line);
         System.out.println("Hello! I'm Pickle");
@@ -14,13 +15,26 @@ public class Pickle {
         while(sc.hasNextLine()) {
             String input = sc.nextLine();
 
+
             if (input.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(line);
                 break;
+            } else if (input.equals("list")) {
+                System.out.println(line);
+                int count = 1;
+                for (int i = 0; i < counter; i++){
+                    System.out.println(count + ". " + list[count - 1]);
+                    count++;
+                }
+
+
+                System.out.println(line);
             } else {
                 System.out.println(line);
-                System.out.println(input);
+                System.out.println("added: " + input);
+                list[counter] = input;
+                counter++;
                 System.out.println(line);
             }
 
