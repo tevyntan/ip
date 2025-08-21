18bfda8d360e37b6143aaff98026928c06f3b59d
tevyntan <tevyntanyb@gmail.com>
2025-08-21 16:13:50 +0800

    Level-0

---
+++ src/main/java/Pickle.java
@@ -0,0 +1,11 @@
+public class Pickle {
+    private static String line = "________________________________________________";
+    public static void main(String[] args) {
+        System.out.println(line);
+        System.out.println("Hello! I'm Pickle");
+        System.out.println("What can I do for you?\n");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
