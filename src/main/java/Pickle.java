aeaffe64ccfaf2c706001c6c91f81afaaf885917
tevyntan <tevyntanyb@gmail.com>
2025-08-22 10:23:10 +0800

    Add support for tracking three types of tasks

--- src/main/java/Pickle.java
+++ src/main/java/Pickle.java
@@ -14,7 +14,7 @@ public class Pickle {
 
         while(sc.hasNextLine()) {
             String inputs = sc.nextLine();
-            String[] input = inputs.split(" ");
+            String[] input = inputs.split(" ",2);
 
 
             if (input[0].equals("bye")) {
@@ -25,7 +25,7 @@ public class Pickle {
                 System.out.println(line);
                 int count = 1;
                 for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(count + ". " + tasks.get(count - 1).toString());
+                    System.out.println(count + "." + tasks.get(count - 1).toString());
                     count++;
                 }
                 System.out.println(line);
@@ -44,11 +44,42 @@ public class Pickle {
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(t.toString());
                 System.out.println(line);
+            } else if (input[0].equals("todo")) {
+                System.out.println(line);
+                counter++;
+                System.out.println("Aights. Task added:");
+                Task t = new ToDos(input[1]);
+                tasks.add(t);
+                System.out.println(t.toString());
+                System.out.println("You got " + counter + " tasks in the list.");
+                System.out.println(line);
+            } else if (input[0].equals("deadline")) {
+                System.out.println(line);
+                counter++;
+                System.out.println("Aights. Task added:");
+                String[] d = input[1].split("/by");
+                Task t = new Deadline(d[0], d[1]);
+                tasks.add(t);
+                System.out.println(t.toString());
+                System.out.println("You got " + counter + " tasks in the list.");
+                System.out.println(line);
+            } else if (input[0].equals("event")) {
+                System.out.println(line);
+                counter++;
+                System.out.println("Aights. Task added:");
+                String[] d = input[1].split("/from");
+                String[] e = d[1].split("/to");
+                Task t = new Event(d[0], e[0], e[1]);
+                tasks.add(t);
+                System.out.println(t.toString());
+                System.out.println("You got " + counter + " tasks in the list.");
+                System.out.println(line);
             } else {
                 System.out.println(line);
                 System.out.println("added: " + inputs);
                 Task t = new Task(inputs);
                 tasks.add(t);
+                counter++;
                 System.out.println(line);
                 }
 
