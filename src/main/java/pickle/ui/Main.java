ea10a7352c2329da1a7d2d65e12e4332d370ce4e
tevyntan <tevyntanyb@gmail.com>
2025-09-05 08:39:00 +0800

    Add GUI name

--- src/main/java/pickle/ui/Main.java
+++ src/main/java/pickle/ui/Main.java
@@ -23,6 +23,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
+            stage.setTitle("Pickle");
             fxmlLoader.<MainWindow>getController().setPickle(pickle);
             stage.show();
 
